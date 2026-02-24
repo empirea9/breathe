d3689e0b9fcc05c74ebc480154ac5faabdc668a2
sidharthify <wednisegit@gmail.com>
2026-02-24 16:13:54 +0530

    breathe: ui: change a few shapes in navbar
    Signed-off-by: sidharthify <wednisegit@gmail.com>

--- app/src/main/java/com/sidharthify/breathe/navigation/Navigation.kt
+++ app/src/main/java/com/sidharthify/breathe/navigation/Navigation.kt
@@ -10,10 +10,10 @@ import androidx.compose.material.icons.outlined.Map
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
-import androidx.compose.material3.MaterialShapes.Companion.Clover4Leaf
-import androidx.compose.material3.MaterialShapes.Companion.Puffy
-import androidx.compose.material3.MaterialShapes.Companion.Slanted
+import androidx.compose.material3.MaterialShapes.Companion.Cookie12Sided
 import androidx.compose.material3.MaterialShapes.Companion.SoftBurst
+import androidx.compose.material3.MaterialShapes.Companion.Square
+import androidx.compose.material3.MaterialShapes.Companion.Slanted
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.graphics.shapes.RoundedPolygon
 
@@ -24,8 +24,8 @@ enum class AppScreen(
     val iconOutlined: ImageVector,
     val shape: RoundedPolygon,
 ) {
-    Home("Home", Icons.Filled.Home, Icons.Outlined.Home, shape = Puffy),
-    Map("Map", Icons.Filled.Map, Icons.Outlined.Map, shape = Slanted),
-    Explore("Explore", Icons.Filled.Search, Icons.Outlined.Search, shape = Clover4Leaf),
+    Home("Home", Icons.Filled.Home, Icons.Outlined.Home, shape = Cookie12Sided),
+    Map("Map", Icons.Filled.Map, Icons.Outlined.Map, shape = Square),
+    Explore("Explore", Icons.Filled.Search, Icons.Outlined.Search, shape = Slanted),
     Settings("Settings", Icons.Filled.Settings, Icons.Outlined.Settings, shape = SoftBurst),
 }
